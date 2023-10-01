9e89714698f87645c33f2799b4625e3e4844e93a
Tom Charon <charon.s.tom@gmail.com>
2023-10-01 21:18:20 +0200

    adjusting some responsives details

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "../components/Nav";
 function Header() {
   return (
     <header className="w-full z-20 shadow-l bg-skin-variant">
-      <div className="container mx-auto flex justify-between items-center py-4 lg:px-10">
+      <div className="container mx-auto flex justify-between items-center py-4 text-sm lg:text-base">
         <div>Task Manager</div>
         <Nav />
       </div>
